c7ae790712b2603916aea1e868c34bafece57ffc
sowji <jujjurisowjanya@gmail.com>
2024-06-12 13:17:21 +0530

    new

---
+++ session-1/ec2/ec2.tf
@@ -0,0 +1,53 @@
+data "aws_ami" "centos8" {
+    owners = ["973714476881"]
+    most_recent      = true
+
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg1.id]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "sg1" {
+  name        = "sg"
+  description = "Allow TLS inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = [0.0.0.0/0]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
